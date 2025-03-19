570e736e8a9436baafb5afb5a657fa4fab98f9e1
Nicole LeGuern <148448915+CodeQueenie@users.noreply.github.com>
2025-03-19 14:22:34 -0400

    Delete setup_github.bat

--- setup_github.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Setting up GitHub repository...
-git remote add origin https://github.com/CodeQueenie/InspireSearch---AI_Powered_Visual_Search_Engine.git
-git branch -M main
-git push -u origin main
-echo Done!
-pause
